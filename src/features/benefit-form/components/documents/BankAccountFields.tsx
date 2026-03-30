ecf6b997c22425ce630ea6c017499b90ba85b037
Pavel Pivovarčík <pavel.pivovarcik@vzp.cz>
2026-03-30 16:39:46 +0200

    fix bank acc err msg

--- src/features/benefit-form/components/documents/BankAccountFields.tsx
+++ src/features/benefit-form/components/documents/BankAccountFields.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Grid, Stack, Text, TextInput } from "@mantine/core";
+import { Box, Group, Stack, Text, TextInput } from "@mantine/core";
 
 import { useBenefitFormContext } from "../../context";
 import { documentsStepTexts } from "./texts";
@@ -9,81 +9,73 @@ export const BankAccountFields: React.FunctionComponent = () => {
 
     return (
         <>
-            <Stack gap="md" hiddenFrom="sm" w="100%">
-                <Grid gutter="xs" align="end">
-                    <Grid.Col span={3}>
+            <Stack gap="md" hiddenFrom="sm">
+                <Group align="flex-start" gap="xs" wrap="nowrap">
+                    <Box maw={96}>
                         <TextInput
                             c="dimmed"
                             label={documentsStepTexts.bankAccountSection.prefixLabel}
                             placeholder={documentsStepTexts.bankAccountSection.prefixPlaceholder}
                             {...form.getInputProps("refundBankAccount.prefix")}
                         />
-                    </Grid.Col>
+                    </Box>
 
-                    <Grid.Col span="content">
-                        <Text aria-hidden="true" role="presentation" c="dimmed" fw={700} mb={10}>
-                            -
-                        </Text>
-                    </Grid.Col>
+                    <Text aria-hidden="true"c="dimmed" fw={700} mt={28}>
+                        -
+                    </Text>
 
-                    <Grid.Col span="auto">
+                    <Box flex="auto">
                         <TextInput
                             c="dimmed"
                             label={documentsStepTexts.bankAccountSection.accountNumberLabel}
                             {...form.getInputProps("refundBankAccount.accountNumber")}
                         />
-                    </Grid.Col>
-                </Grid>
+                    </Box>
+                </Group>
 
-                <Grid>
-                    <Grid.Col span={{ base: 12, xs: 6 }}>
-                        <TextInput
-                            c="dimmed"
-                            label={documentsStepTexts.bankAccountSection.bankCodeLabel}
-                            {...form.getInputProps("refundBankAccount.bankCode")}
-                        />
-                    </Grid.Col>
-                </Grid>
+                <Box >
+                    <TextInput
+                        c="dimmed"
+                        label={documentsStepTexts.bankAccountSection.bankCodeLabel}
+                        {...form.getInputProps("refundBankAccount.bankCode")}
+                    />
+                </Box>
             </Stack>
 
-            <Grid gutter="xs" align="end" visibleFrom="sm">
-                <Grid.Col span={2}>
+            <Group align="flex-start" gap="xs"  visibleFrom="sm">
+                <Box maw={100}>
                     <TextInput
                         c="dimmed"
                         label={documentsStepTexts.bankAccountSection.prefixLabel}
                         placeholder={documentsStepTexts.bankAccountSection.prefixPlaceholder}
                         {...form.getInputProps("refundBankAccount.prefix")}
                     />
-                </Grid.Col>
+                </Box>
 
-                <Grid.Col span="content">
-                    <Text aria-hidden="true" role="presentation" c="dimmed" fw={700} mb={10}>
-                        -
-                    </Text>
-                </Grid.Col>
+                <Text aria-hidden="true" c="dimmed" fw={700} mt={28}>
+                    -
+                </Text>
 
-                <Grid.Col span={5}>
+                <Box flex="auto">
                     <TextInput
                         c="dimmed"
                         label={documentsStepTexts.bankAccountSection.accountNumberLabel}
                         {...form.getInputProps("refundBankAccount.accountNumber")}
                     />
-                </Grid.Col>
+                </Box>
 
-                <Grid.Col span="content">
-                    <Text aria-hidden="true" role="presentation" c="dimmed" fw={700} mb={10}>
-                        /
-                    </Text>
-                </Grid.Col>
+                <Text aria-hidden="true" c="dimmed" fw={700} mt={28}>
+                    /
+                </Text>
 
-                <Grid.Col span={4}>
+                <Box >
                     <TextInput
                         c="dimmed"
                         label={documentsStepTexts.bankAccountSection.bankCodeLabel}
                         {...form.getInputProps("refundBankAccount.bankCode")}
                     />
-                </Grid.Col>
-            </Grid>
+                </Box>
+            </Group>
         </>
     );
 };
